38b0cf1005ebdc6dca7643d54f0e22deab9fead8
DogeAmarisho <elbot9425@gmail.com>
2026-05-12 11:58:09 -0400

    cdcjd

--- routes/catalogoRoutes.js
+++ routes/catalogoRoutes.js
@@ -1,7 +1,7 @@
 // routes/catalogoRoutes.js
 const express = require('express');
 const router = express.Router();
-const ScraperCtrl = require('../controllers/scraperController');
+const Ctrl = require('../controllers/scraperController');
 
 router.get('/scrape', Ctrl.obtenerDatos);
 module.exports = router;
